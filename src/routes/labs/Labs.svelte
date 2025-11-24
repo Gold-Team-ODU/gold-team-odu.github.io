421eda36fea2b299049bc09d0e41a259271ba89a
Brennen Gabriel <bgabr001@odu.edu>
2025-11-24 14:21:47 -0500

    Added Brennen Labs to the website

--- src/routes/labs/Labs.svelte
+++ src/routes/labs/Labs.svelte
@@ -96,6 +96,32 @@
           <!-- END REGION TO COPY -->
         </Accordion>
       </Tabs.Panel>
+
+      <!-- Lab 2 Panel -->
+      <Tabs.Panel value="lab1">
+        <Accordion {value} onValueChange={(e) => (value = e.value)} collapsible>
+          
+          <!-- COPY THIS FOR EACH MEMBER. (Change value to be unique and use names accordingly)-->
+
+          <Accordion.Item value="Brennen_Gabriel">
+            <!-- Control -->
+            {#snippet control()}Brennen Gabriel{/snippet}
+            <!-- Panel -->
+            {#snippet panel()}
+              <article class="space-y-2 p-4">
+                <embed
+                  src="static/labs/Lab2_rogulelike_algebra_Brennen_version_2.pdf"
+                  type="application/pdf"
+                  class="h-[calc(100vh-25rem)] w-full"
+                />
+              </article>
+            {/snippet}
+          </Accordion.Item>
+
+          <!-- END REGION TO COPY -->
+        </Accordion>
+      </Tabs.Panel>
+          
       <Tabs.Panel value="lab34">
         <article class="space-y-2 p-4">
           Link to User Manual:
